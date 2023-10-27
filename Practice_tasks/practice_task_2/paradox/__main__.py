6bd3b2c7b9105066460f6687778367da8bdf3b4a
artemii <kavrese@gmail.com>
2023-10-27 11:20:42 +0300

    refactored second practice

--- Practice_tasks/practice_task_2/paradox/__main__.py
+++ Practice_tasks/practice_task_2/paradox/__main__.py
@@ -2,9 +2,25 @@ import sys
 
 from Practice_tasks.practice_task_2.paradox import birthday, monty_hall
 
+
+def input_with_default(text, default):
+    origin_input = input(text)
+    if origin_input == "":
+        return default
+    return origin_input
+
+
+def run_birthday():
+    iterations = int(input_with_default("Введите кол-во итераций: ", 1000))
+    first_group = int(input_with_default("Введите кол-во людей в первой группе: ", 63))
+    second_group = int(input_with_default("Введите кол-во людей во второй группе: ", 20))
+    print("Начало генерации... Ожидайте...")
+    return birthday(iterations, first_group, second_group)
+
+
 runs = {
     1: lambda: print(monty_hall(int(input("Введите кол-во итераций: ")))),
-    2: lambda: print(birthday(int(input("Введите кол-во итераций: ")))),
+    2: lambda: print(run_birthday()),
     3: sys.exit
 }
 
--- Practice_tasks/practice_task_2/paradox/birthday_paradox/paradox.py
+++ Practice_tasks/practice_task_2/paradox/birthday_paradox/paradox.py
@@ -1,21 +1,22 @@
 from Practice_tasks.practice_task_2.paradox.birthday_paradox.models import Group
 
 
-def birthday(count_groups: int) -> str:
+def birthday(count_groups: int, count_first: int, count_second: int) -> str:
     """
     Проверка парадокса:
-    Генерируем 1000 групп с 23-мя днями рождения и считаем вероятность наличия хотя бы одной пары
-    одинаковых дней рождения.
-    Повторяем для групп с 60-ю днями рождения.
+    Генерируем две группы с днями рождения и считаем вероятность наличия хотя бы одной пары
+    одинаковых дней рождения в одной группе.
     :return: Вероятности
     :param count_groups: Кол-во генерируемых групп для одного типа (с 60-ю и с 23-я)
+    :param count_first: Кол-во людей в первой группе
+    :param count_second: Кол-во людей во второй группе
     """
-    groups_23 = Group.generate_groups(count_groups, 23)
-    groups_60 = Group.generate_groups(count_groups, 60)
+    groups_1 = Group.generate_groups(count_groups, count_first)
+    groups_2 = Group.generate_groups(count_groups, count_second)
     return (f"Вероятность нахождения хоть одной пары с одинаковыми днями рождения: \n"
-            f"У групп с 23-мя днями - {Group.get_probability_equal_birthday_in_groups(groups_23)}%\n"
-            f"У групп с 60-ю днями - {Group.get_probability_equal_birthday_in_groups(groups_60)}%")
+            f"У первой группы ({count_first} чел.) - {Group.get_probability_equal_birthday_in_groups(groups_1)}%\n"
+            f"У первой группы ({count_second} чел.) - {Group.get_probability_equal_birthday_in_groups(groups_2)}%")
 
 
 if __name__ == "__main__":
-    birthday(1000)
+    birthday(1000, 60, 23)
