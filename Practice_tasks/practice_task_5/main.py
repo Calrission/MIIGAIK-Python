47a709fdd61a40aaf5cf9b44faaff52ae5921baa
artemii <kavrese@gmail.com>
2023-11-02 15:52:09 +0300

    completed practice 5

---
+++ Practice_tasks/practice_task_5/main.py
@@ -0,0 +1,25 @@
+def read_file(file_name: str) -> list[int]:
+    file = None
+    try:
+        file = open(file_name, "r")
+        nums = []
+        for i in file.read().split("\n"):
+            num = int(i)
+            nums.append(num)
+        file.close()
+        return nums
+    except FileNotFoundError:
+        print(f"{file_name} не существует")
+    except IOError:
+        print("IO ошибка")
+    except:
+        print("Другие ошибки")
+    finally:
+        if file is not None:
+            file.close()
+    return []
+
+
+if __name__ == "__main__":
+    data = read_file(input("Введите имя файла: "))
+    print(data)
