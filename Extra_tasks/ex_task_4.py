6113bcf9527ed34a4dfb3389e12fd7a040d4f373
artemii <kavrese@gmail.com>
2023-11-16 20:12:22 +0300

    completed ex_task_4

---
+++ Extra_tasks/ex_task_4.py
@@ -0,0 +1,12 @@
+import urllib.request
+import re
+import csv
+
+regex = r"(?:org-widget-header__title\">[^\/]+)(?P<url>[^\"]+)(?:[^>]+>)(?P<title>[^<]+)(?:[^\n]+)(?:(?:(?:[^>]+>){2}\n[^<]+<\/span>\n[^<]+<span class=\"org-widget-header__meta org-widget-header__meta--location\">\n|(?:[^>]+>){2}\n))(?:\s+)(?P<address>[^\n]+)(?:[^+Т]+)Телефон(?:(?:[^>]+>){3})(?P<phones>[\d (),+-]+)(?:[^\n]+\n){4}(?:[^>]+>)(?P<time_work>[^<]+|)"
+response = urllib.request.urlopen("https://msk.spravker.ru/avtoservisy-avtotehcentry").read().decode()
+results = re.findall(regex, response)
+
+with open("results.csv", "w") as file:
+    writer = csv.writer(file, delimiter=",")
+    writer.writerow(("url_spravker", "title", "address", "phones", "word_time"))
+    writer.writerows(results)
