852262f5cf236383fcb0c284aaeb1edb6f4cc22e
artemii <kavrese@gmail.com>
2023-11-17 10:38:41 +0300

    Revert "small reformatted regex in ex_task_4.py"
    This reverts commit 34f49104fc573f49d804c12013c07d82f4e3d19a.

--- Extra_tasks/ex_task_4.py
+++ Extra_tasks/ex_task_4.py
@@ -2,12 +2,7 @@ import urllib.request
 import re
 import csv
 
-regex = r"""
-(?:org-widget-header__title\">[^\/]+)(?P<url>[^\"]+)(?:[^>]+>)(?P<title>[^<]+)
-(?:[^\n]+)(?:(?:(?:[^>]+>){2}\n[^<]+<\/span>\n[^<]+
-<span class=\"org-widget-header__meta org-widget-header__meta--location\">\n|
-(?:[^>]+>){2}\n))(?:\s+)(?P<address>[^\n]+)(?:[^+Т]+)Телефон(?:(?:[^>]+>){3})(?P<phones>[\d (),+-]+)
-(?:[^\n]+\n){4}(?:[^>]+>)(?P<time_work>[^<]+|)"""
+regex = r"(?:org-widget-header__title\">[^\/]+)(?P<url>[^\"]+)(?:[^>]+>)(?P<title>[^<]+)(?:[^\n]+)(?:(?:(?:[^>]+>){2}\n[^<]+<\/span>\n[^<]+<span class=\"org-widget-header__meta org-widget-header__meta--location\">\n|(?:[^>]+>){2}\n))(?:\s+)(?P<address>[^\n]+)(?:[^+Т]+)Телефон(?:(?:[^>]+>){3})(?P<phones>[\d (),+-]+)(?:[^\n]+\n){4}(?:[^>]+>)(?P<time_work>[^<]+|)"
 
 with open("results.csv", "a+") as file:
     writer = csv.writer(file, delimiter=",")
