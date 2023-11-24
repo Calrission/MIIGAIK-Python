5d31fb724dc8ef43b51fa7767065a182af857bab
artemii <kavrese@gmail.com>
2023-11-24 10:55:52 +0300

    refactored

--- main.py
+++ main.py
@@ -3,24 +3,38 @@ import csv
 from translate import Translator
 
 
-with open("dialogs.txt", mode="r") as file:
-    words = file.read().split("\n")
-
-morph = pymorphy3.MorphAnalyzer()
-words = [morph.parse(word)[0].normal_form for word in words]
-
-words_counts = {word: words.count(word) for word in set(words)}
-words_counts = {key: words_counts[key] for key in sorted(words_counts, key=lambda x: words_counts[x], reverse=True)}
-
-translator = Translator(from_lang="ru", to_lang="en")
-
-with open("translated.txt", mode="w") as file:
-    writer = csv.writer(file, delimiter="|")
-    writer.writerow("Исходное слово | Перевод | Количество упоминаний".split(" | "))
-    for index, pair in enumerate(words_counts.items()):
-        word, count = pair
-        translated_word = translator.translate(word)
-        writer.writerow([word, translated_word.lower(), count])
-        print(f"Переведено {index + 1}/{len(words_counts)}")
+def get_words() -> list[str]:
+    with open("dialogs.txt", mode="r") as f:
+        words = f.read().split("\n")
+    return words
 
 
+def to_normal(words: list[str]) -> list[str]:
+    morph = pymorphy3.MorphAnalyzer()
+    words = [morph.parse(word)[0].normal_form for word in words]
+    return words
+
+
+def to_dict(lst: list[str]) -> dict[str, int]:
+    result = {word: lst.count(word) for word in set(lst)}
+    result = {key: result[key] for key in sorted(result, key=lambda x: result[x], reverse=True)}
+    return result
+
+
+def translate_and_save(data: dict[str, int]):
+    translator = Translator(from_lang="ru", to_lang="en")
+    with open("translated.csv", mode="w") as file:
+        writer = csv.writer(file, delimiter="|")
+        writer.writerow("Исходное слово | Перевод | Количество упоминаний".split(" | "))
+        for index, pair in enumerate(data.items()):
+            word, count = pair
+            translated_word = translator.translate(word)
+            writer.writerow([word, translated_word.lower(), count])
+            print(f"Переведено {index + 1}/{len(data)}")
+
+
+if __name__ == "__main__":
+    origin_words = get_words()
+    normals_words = to_normal(origin_words)
+    words_counts = to_dict(normals_words)
+    translate_and_save(words_counts)
